32964e78c90cc9741b194b9d273d71c43a301a05
Sumona akter juthi <113887905+juthi456@users.noreply.github.com>
2022-12-12 14:08:35 +0600

    Update AdminHome (1).php

--- final lab exam/AdminHome (1).php
+++ final lab exam/AdminHome (1).php
@@ -1,19 +1,41 @@
 <html>
+
 <head>
+
     <title>Admin Home Page</title>
+
 </head>
+
     <body>
+
             <form method="" action="" enctype="">
+
                     <fieldset align="center">
+
                         <legend>Admin Home Page </legend>
-                            <?php
-                                echo "<h2>Welcome {$_SESSION['name']}!!<h2>";
-                            ?>
-                            
+
+                           
+
+                             
+
+                            <legend>Profile </legend>
+
+                            <legend>change password </legend>
+
+                            <legend>view users </legend>
+
+                            <legend>Logout </legend>
+
                             <br><hr>
-                            <input type="submit" name="" value="login">&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;<a href="Registration.php">Registraion</a>
+
+                           
+
+
 
                     </fieldset>
+
             </form>
+
     </body>
+
 </html>
